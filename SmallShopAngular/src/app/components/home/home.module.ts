777f1783f6fbc032d84a53d47407d43ef74c8316
DenisRudiy <denryd2311@gmail.com>
2023-02-18 20:42:47 +0200

    small changes

--- SmallShopAngular/src/app/components/home/home.module.ts
+++ SmallShopAngular/src/app/components/home/home.module.ts
@@ -6,11 +6,10 @@ import { MatIconModule } from '@angular/material/icon'
 
 import { HomeComponent } from './home.component'
 import { FoodItemComponent } from './food-item/food-item.component'
-import { SearchComponent } from './search/search.component';
-import { FiltersComponent } from './filters/filters.component'
+import { SearchComponent } from './search/search.component'
 
 @NgModule({
-  declarations: [HomeComponent, FoodItemComponent, SearchComponent, FiltersComponent],
+  declarations: [HomeComponent, FoodItemComponent, SearchComponent],
   imports: [CommonModule, RouterModule, FormsModule, MatIconModule]
 })
 export class HomeModule {}
